5f1e617bdd34bc31239015c850b5c85afa20a423
s0044613 <sama.mushtaq@rgare.com>
2020-02-18 09:37:43 -0600

    remove dummy data

--- src/app.js
+++ src/app.js
@@ -13,16 +13,9 @@ import 'react-dates/lib/css/_datepicker.css';
 
 
 const store = configureStore();//create the store
-store.dispatch(addExpense({ desc: 'water bill', note: 'water bill for the month of Jan', amount: 20, createdAt: 450000000 }));
-
-console.log(store.getState());
-store.dispatch(addExpense({ desc: 'gas bill', note: 'gas bill for the month of Jan', amount: 40, createdAt: 800000000 }));
-
-store.dispatch(addExpense({ desc: 'rent', note: 'rent', amount: 440, createdAt: 10000000 }));
-
 //store.dispatch(setTextFilter('gas'));
 const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+
 
 //The <Provider /> makes the Redux store available to any nested components that have been wrapped in the connect() function.
 const jsx = (
