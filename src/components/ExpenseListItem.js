af39eaf7b0aa9706b1a02e4396d30da18f61333a
s0044613 <sama.mushtaq@rgare.com>
2020-02-19 14:13:34 -0600

    updated expenselistitem and test

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import moment from 'moment';
+import numeral from 'numeral';
 
-const ExpenseListItem = ({id, desc, amount, createdAt }) => (
+const ExpenseListItem = ({ id, desc, amount, createdAt }) => (
     <div>
-        <Link to={`/edit/${id}`}> <h3>{desc}</h3></Link>
-        <div> {amount} {createdAt}</div>
+        <Link to={`/edit/${id}`}>
+            <h3>{desc}</h3>
+        </Link>
+        <p>
+            {numeral(amount /100 ).format('$0,0.00')}
+            -
+            {moment(createdAt).format('MMMM Do, YYYY')}
+        </p>
     </div>
 );
 
